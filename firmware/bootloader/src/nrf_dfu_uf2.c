d8760702c1fac12a213b8bc2e3ae9425f19745cf
Niel Nielsen <nieldk@gmail.com>
2026-05-19 09:52:20 +0200

    Update nrf_dfu_uf2.c

--- firmware/bootloader/src/nrf_dfu_uf2.c
+++ firmware/bootloader/src/nrf_dfu_uf2.c
@@ -48,15 +48,12 @@ NRF_LOG_MODULE_REGISTER();
 #include "uf2_blockdev.h"
 
 /* ---- Endpoint plan ----
- * The CDC ACM transport in nrf_dfu_serial_usb.c uses:
- *     interface 0   EPIN2          (COMM)
- *     interface 1   EPIN1, EPOUT1  (DATA)
- * Pick clear endpoints for MSC:
- *     interface 2   EPIN3, EPOUT2
+ * CDC has been dropped from the bootloader build, so we own the entire
+ * USBD device and use the conventional MSC-on-EP1 layout.
  * ----------------------- */
-#define MSC_INTERFACE        2
-#define MSC_EPIN             NRF_DRV_USBD_EPIN3
-#define MSC_EPOUT            NRF_DRV_USBD_EPOUT2
+#define MSC_INTERFACE        0
+#define MSC_EPIN             NRF_DRV_USBD_EPIN1
+#define MSC_EPOUT            NRF_DRV_USBD_EPOUT1
 #define MSC_WORKBUFFER_SIZE  512
 
 static nrf_dfu_observer_t m_observer;
