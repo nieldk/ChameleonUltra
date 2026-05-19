f1da77063d7cdff97f9043df50235a69e11f2158
Niel Nielsen <nieldk@gmail.com>
2026-05-19 08:50:59 +0200

    Update sdk_config.h

--- firmware/bootloader/src/sdk_config.h
+++ firmware/bootloader/src/sdk_config.h
@@ -1276,7 +1276,7 @@
 // <i> This reduced feature set is used by the BLE transport to reduce flash usage.
 
 #ifndef NRF_DFU_PROTOCOL_REDUCED
-#define NRF_DFU_PROTOCOL_REDUCED 1
+#define NRF_DFU_PROTOCOL_REDUCED 0
 #endif
 
 // <q> NRF_DFU_PROTOCOL_VERSION_MSG  - Protocol version message support.
