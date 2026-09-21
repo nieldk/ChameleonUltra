eb6bf1ac791178e78bf0b9c7ccaeeac5513fd1d0
Niel Nielsen <nieldk@gmail.com>
2026-09-21 09:49:15 +0200

    Remove indala_get_debug function declaration

--- firmware/application/src/rfid/nfctag/lf/protocols/indala.h
+++ firmware/application/src/rfid/nfctag/lf/protocols/indala.h
@@ -8,6 +8,5 @@
 extern const protocol indala;
 
 uint8_t indala_t55xx_writer(uint8_t* uid, uint32_t* blks);
-void indala_get_debug(uint8_t *off, uint16_t *nb, int32_t *mag);
 
 #endif /* INDALA_H_ */
